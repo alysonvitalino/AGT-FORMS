a435108d718722224391945050f06fc5fc6a0ff3
Alyson Vitalino <alyson.vitalino@gmail.com>
2025-04-07 19:02:50 -0300

    Renomeando arquivos

--- FUNCIONARIOS/FUNCIONARIOS/Properties/Resources.Designer.cs
+++ FUNCIONARIOS/FUNCIONARIOS/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FUNCIONARIOS.Properties {
+namespace AGT_FORMS.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace FUNCIONARIOS.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FUNCIONARIOS.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AGT_FORMS.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
