60fcada5ef547ae25a9df483440363b79848a5ed
Aluno <alyson.vitalino@gmail.com>
2025-04-24 21:40:16 -0300

    Pequenos ajustes

--- FUNCIONARIOS/FUNCIONARIOS/HomePage.Designer.cs
+++ FUNCIONARIOS/FUNCIONARIOS/HomePage.Designer.cs
@@ -192,12 +192,11 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(30, 87);
+            label1.Location = new Point(12, 50);
             label1.Name = "label1";
             label1.Size = new Size(1158, 25);
             label1.TabIndex = 30;
             label1.Text = "COLOCAR O LOGO DA EMPRESA OU GRÁFICOS ILUSTRADOS, ADICIONAR DESCRIÇÕES INTERATIVAS NOS BOTÕES EXPLICANDO AS FUNÇÕES";
-            label1.Click += label1_Click;
             // 
             // HomePage
             // 
