905e07dfcecb6b23af991c580594d5b2b2e32dc0
aluno <alyson.vitalino@gmail.com>
2025-05-19 21:58:27 -0300

    Pequenos ajustes

--- FUNCIONARIOS/FUNCIONARIOS/LoginsEditar.Designer.cs
+++ FUNCIONARIOS/FUNCIONARIOS/LoginsEditar.Designer.cs
@@ -29,6 +29,27 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            textBox9 = new TextBox();
+            textBox8 = new TextBox();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            button2 = new Button();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
             SuspendLayout();
             // 
             // button1
@@ -41,19 +62,236 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(515, 279);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(100, 23);
+            textBox9.TabIndex = 37;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(349, 279);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 23);
+            textBox8.TabIndex = 36;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(177, 279);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(119, 23);
+            textBox7.TabIndex = 35;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(515, 207);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 34;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(349, 207);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 33;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(177, 207);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(119, 23);
+            textBox4.TabIndex = 32;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(515, 135);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 31;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(349, 135);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 30;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(177, 135);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(119, 23);
+            textBox1.TabIndex = 29;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(515, 189);
+            label10.Name = "label10";
+            label10.Size = new Size(40, 15);
+            label10.TabIndex = 28;
+            label10.Text = "Login:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(515, 261);
+            label9.Name = "label9";
+            label9.Size = new Size(72, 15);
+            label9.TabIndex = 27;
+            label9.Text = "Observação:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(349, 261);
+            label8.Name = "label8";
+            label8.Size = new Size(73, 15);
+            label8.TabIndex = 26;
+            label8.Text = "Vencimento:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(349, 189);
+            label7.Name = "label7";
+            label7.Size = new Size(29, 15);
+            label7.TabIndex = 25;
+            label7.Text = "Site:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(177, 261);
+            label6.Name = "label6";
+            label6.Size = new Size(42, 15);
+            label6.TabIndex = 24;
+            label6.Text = "Senha:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(177, 189);
+            label5.Name = "label5";
+            label5.Size = new Size(51, 15);
+            label5.TabIndex = 23;
+            label5.Text = "Sistema:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(515, 113);
+            label4.Name = "label4";
+            label4.Size = new Size(37, 15);
+            label4.TabIndex = 22;
+            label4.Text = "CNPJ:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(349, 113);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 15);
+            label3.TabIndex = 21;
+            label3.Text = "Munícipio:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(177, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(112, 15);
+            label2.TabIndex = 20;
+            label2.Text = "Código da Unidade:";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(331, 326);
+            button2.Name = "button2";
+            button2.Size = new Size(130, 84);
+            button2.TabIndex = 38;
+            button2.Text = "Atualizar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.BackColor = Color.WhiteSmoke;
+            comboBox1.ForeColor = Color.MidnightBlue;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(177, 62);
+            comboBox1.Margin = new Padding(1, 2, 1, 2);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(438, 23);
+            comboBox1.TabIndex = 39;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(331, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(116, 15);
+            label1.TabIndex = 40;
+            label1.Text = "Selecione a Unidade:";
+            // 
             // LoginsEditar
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Controls.Add(button2);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(button1);
             Name = "LoginsEditar";
             Text = "LoginsEditar";
+            Load += LoginsEditar_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private TextBox textBox9;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Button button2;
+        private ComboBox comboBox1;
+        private Label label1;
     }
 }
--- FUNCIONARIOS/FUNCIONARIOS/LoginsEditar.cs
+++ FUNCIONARIOS/FUNCIONARIOS/LoginsEditar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,13 +17,44 @@ namespace AGT_FORMS
         {
             InitializeComponent();
         }
+        private void CarregarComboBox()
+        {
+            string query = "SELECT unidade_cadastro, municipio_cadastro, cnpj_cadastro FROM cadastro ORDER BY unidade_cadastro ASC";
 
+            try
+            {
+                using (MySqlConnection conexao = DBHelper.ObterConexao())
+                {
+                    using (MySqlCommand cmd = new MySqlCommand(query, conexao))
+                    {
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            comboBox1.Items.Clear();
+                            while (reader.Read())
+                            {
+                                string item = reader["unidade_cadastro"].ToString() + " - " + reader["municipio_cadastro"].ToString() + " - " + reader["cnpj_cadastro"].ToString();
+                                comboBox1.Items.Add(item);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao carregar ComboBox: " + ex.Message);
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             Logins_Senhas produto = new Logins_Senhas();
             produto.StartPosition = FormStartPosition.CenterScreen;
             produto.Show();
             Hide();
+        }
+
+        private void LoginsEditar_Load(object sender, EventArgs e)
+        {
+            CarregarComboBox();
         }
     }
 }
